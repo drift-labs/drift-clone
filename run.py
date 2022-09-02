52ac4a98be6c07a45c229fb4bfc24c99ae125062
0xNineteen <0x39015319@gmail.com>
2022-09-02 19:11:39 -0400

    progress

--- run.py
+++ run.py
@@ -89,6 +89,8 @@ for p in pathlib.Path('keypairs/').iterdir():
         ch = ClearingHouse.from_config(config, provider)
         chs.append(ch)
 
+assert state_kp is not None
+
 #%%
 await state_ch.update_auction_duration(0, 0)
 await state_ch.update_max_base_asset_amount_ratio(1, 0)
@@ -97,28 +99,41 @@ await state_ch.update_market_base_asset_amount_step_size(1, 0)
 #%%
 from tqdm.notebook import tqdm 
 
-total_baa = 0 
+net_baa = 0 
 sigs = []
 for ch in tqdm(chs):
     user = await get_user_account(
         ch.program, 
         ch.authority
     )
-    position = [p for p in user.positions if p.market_index == 0][0]
-    baa = position.base_asset_amount
+    position = [p for p in user.positions if p.market_index == 0 and (p.base_asset_amount != 0 or p.lp_shares > 0)]
 
-    if position.lp_shares > 0:
-        print('removeing...', position.lp_shares)
-        sig = await ch.remove_liquidity(position.lp_shares, 0)
-        sigs.append(sig)
+    if len(position) > 0:
+        assert len(position) == 1
+        position = position[0]
+        baa = position.base_asset_amount
 
-    if baa != 0:
-        print('closing...', baa/1e13)
-        sig = await ch.close_position(0)
-        sigs.append(sig)
-        total_baa += abs(baa)
+        # if position.lp_shares > 0:
+        #     print('removing...', position.lp_shares)
+        #     sig = await ch.remove_liquidity(position.lp_shares, 0)
+        #     sigs.append(sig)
 
-total_baa
+        if baa != 0:
+            print('closing...', baa/1e13)
+            sig = await ch.close_position(0)
+            sigs.append(sig)
+            net_baa += baa
+
+net_baa
+
+# #%%
+# await ch.add_liquidity(100, 0)
+
+#%%
+market = await get_market_account(
+    ch.program, 0
+)
+market.amm.net_base_asset_amount
 
 #%%
 while True:
@@ -137,10 +152,13 @@ for ch in tqdm(chs):
         ch.program, 
         ch.authority
     )
-    baa = [p for p in user.positions if p.market_index == 0][0].base_asset_amount
-    if baa != 0:
+    position = [p for p in user.positions if p.market_index == 0 and (p.base_asset_amount != 0 or p.lp_shares > 0)]
+    if len(position) > 0:
+        assert len(position) == 1
+        position = position[0]
+        baa = position.base_asset_amount
         print('baa:', baa/1e13)
-    total_baa += abs(baa)
+        total_baa += abs(baa)
 total_baa
 
 #%%
@@ -154,6 +172,26 @@ market.amm.net_base_asset_amount
 validator.stop()
 
 # %%
+config = configs['devnet']
+url = 'https://api.devnet.solana.com'
+wallet = Wallet(state_kp)
+connection = AsyncClient(url)
+provider = Provider(connection, wallet)
+ch = ClearingHouse.from_config(config, provider)
 
+# %%
+# from driftpy.src.driftpy.types import Market
+market = await get_market_account(
+    ch.program, 
+    0
+)
+net_baa = market.amm.net_base_asset_amount + market.amm.net_unsettled_lp_base_asset_amount
+bar = market.amm.base_asset_reserve
+qar = market.amm.quote_asset_reserve
+
+# net_baa > 0 then users need to short & so net_baa + bar = tbar 
+# net-baa < 0 then users need to long & so net_baa - b
+net_baa + bar < market.amm.max_base_asset_reserve
+net_baa + bar > market.amm.min_base_asset_reserve
 
 # %%
--- scrape.py
+++ scrape.py
@@ -53,7 +53,8 @@ accounts_dir.mkdir(parents=True, exist_ok=True)
 keypairs_dir.mkdir(parents=True, exist_ok=True)
 
 config = configs['devnet']
-url = 'https://api.devnet.solana.com'
+# url = 'https://api.devnet.solana.com'
+url = "http://3.220.170.22:8899"
 wallet = Wallet(state_kp)
 connection = AsyncClient(url)
 provider = Provider(connection, wallet)
@@ -71,6 +72,10 @@ def save_account_info(
         'account': account_info,
         'pubkey': pubkey
     }
+
+    if path.exists(): 
+        print(f'overwriting path {path}...')
+
     with open(path, 'w') as f: 
         json.dump(local_account, f)
     
@@ -78,14 +83,25 @@ async def batch_get_account_infos(
     addresses,
     batch_size = 100,
 ):
+    _slot = None
+    is_same_slot = True
     account_infos = []
     for i in tqdm(range(0, len(addresses), batch_size)):
         batch_addresses = addresses[i: i+batch_size]
+        # TODO: batch these multi account info requests?
         batch_account_infos = (await connection.get_multiple_accounts(
             batch_addresses
-        ))['result']['value']
-        account_infos += batch_account_infos
-    return account_infos
+        ))['result']
+        slot = batch_account_infos['context']['slot']
+        if _slot == None: 
+            _slot = slot 
+        elif slot != _slot: 
+            print(_slot, slot)
+            is_same_slot = False
+
+        account_infos += batch_account_infos['value']
+
+    return account_infos, is_same_slot
 
 def init_account_dir(account_type: str):
     path = accounts_dir/account_type
@@ -107,7 +123,7 @@ async def download_all_accounts(
 
     print(f'found {len(accounts)} accounts...')
     
-    account_infos = await batch_get_account_infos(
+    account_infos, _ = await batch_get_account_infos(
         addresses,
         batch_size
     )
@@ -119,41 +135,193 @@ async def download_all_accounts(
             str(pubkey)
         )
 
-## clone with no mods
-types = [
-    "Bank", 
-    "Market", 
-]
-for account_type in types: 
-    print(f'saving account type: {account_type}')
-    await download_all_accounts(account_type)
+#%%
+async def get_all_pks(type):
+    accounts = await ch.program.account[type].all()
+    addrs = [u.public_key for u in accounts]
+    indexs = [len(addrs)]
+    types = [type]
+    
+    if type == 'Market':
+        oracles = [a.account.amm.oracle for a in accounts]
+        addrs += oracles
+        indexs.append(len(addrs))
+        types.append("Oracles")
+    elif type == 'Bank':
+        oracles = [a.account.oracle for a in accounts]
+        addrs += oracles
+        indexs.append(len(addrs))
+        types.append("Oracles")
+
+    return addrs, indexs, types
+
+types = []
+indexs = []
+addrs = []
+for k in ch.program.account.keys():
+    k_addrs, k_indexs, k_types = await get_all_pks(k)
+    indexs += [i + len(addrs) for i in k_indexs]
+    addrs += k_addrs
+    types += k_types
+
+len(addrs), indexs, types
 
 #%%
-## change the admin 
-lamports = 6674640
-state_accounts = await ch.program.account["State"].all()
-assert len(state_accounts) == 1
-state = state_accounts[0]
+success = False 
+while not success:
+    account_infos, success = await batch_get_account_infos(
+        addrs, 
+        batch_size=40
+    )
+    time.sleep(2)
+len(account_infos), success
 
-state_path = init_account_dir("State")
+#%%
+for addr, acc in zip(addrs, account_infos):
+    print(addr, acc)
 
-state.account.admin = state_kp.public_key
+#%%
+type_accounts = {}
 
-anchor_state = Instruction(data=state.account, name="State")
-data = ch.program.account["State"]._coder.accounts.build(anchor_state)
-acc_data = base64.b64encode(data).decode("utf-8")
-account_info = {
-    'data': [acc_data, 'base64'],
-    'executable': False,
-    'lamports': lamports,
-    'owner': str(ch.program_id),
-    'rentEpoch': 367
-}
-save_account_info(
-    state_path/(str(state.public_key) + '.json'), 
-    account_info, 
-    str(state.public_key)
-)
+def decode(type, data):
+    data = base64.b64decode(data)
+    account = ch.program.account[type]._coder.accounts.parse(data).data
+    return account
+
+def encode(type, account):
+    anchor_data = Instruction(data=account, name=type)
+    data = ch.program.account[type]._coder.accounts.build(anchor_data)
+    data = base64.b64encode(data).decode("utf-8")
+    return data
+
+do_nothing_types = [
+    "Bank",
+    "Market",
+    "Oracles"
+]
+
+for i in range(len(types)):
+    type = types[i]
+    if i == 0:
+        acc_info = account_infos[:indexs[i]]
+        addr_info = addrs[:indexs[i]]
+    else:
+        acc_info = account_infos[indexs[i-1]:indexs[i]]
+        addr_info = addrs[indexs[i-1]:indexs[i]] 
+
+    if type in do_nothing_types:
+        print(f'saving {len(acc_info)} without mod: {type}...')
+        assert len(acc_info) == len(addr_info)
+        state_path = init_account_dir(type)
+        for acc, addr in zip(acc_info, addr_info):
+            print(f'saving addr {addr}')
+            save_account_info(
+                state_path/(str(addr) + '.json'), 
+                acc, 
+                str(addr)
+            )
+    else:
+        for addr, enc_account in zip(addr_info, acc_info):
+            data = enc_account['data'][0]
+            enc_account['decoded_data'] = decode(type, data)
+            enc_account['addr'] = addr
+
+        type_accounts[type] = acc_info
+
+#%%
+## dont do anything to the market or bank 
+
+def state_mod(state):
+    state.admin = state_kp.public_key
+
+def modify_and_save_account(
+    type, 
+    mod_fcn
+):
+    state_path = init_account_dir(type)
+
+    accounts = type_accounts[type]
+    for account_dict in accounts:
+        obj = account_dict.pop('decoded_data')
+        addr: PublicKey = account_dict.pop('addr')
+
+        new_addr = mod_fcn(obj)
+        if new_addr is not None:
+            addr = new_addr
+
+        account_dict['data'][0] = encode(type, obj)
+        save_account_info(
+            state_path/(str(addr) + '.json'), 
+            account_dict, 
+            str(addr)
+        )
+
+auth_to_new_kp = {}
+def user_user_stats_mod(
+    type,
+    user_or_stats,
+):
+    global auth_to_new_kp
+
+    old_admin = user_or_stats.authority
+    if str(old_admin) in auth_to_new_kp:
+        kp = auth_to_new_kp[str(old_admin)]
+    else:
+        kp = Keypair()
+        auth_to_new_kp[str(old_admin)] = kp
+        with open(keypairs_dir/f'{kp.public_key}.secret', 'w') as f: 
+            f.write(kp.secret_key.hex())
+    
+    user_or_stats.authority = kp.public_key
+
+    if type == 'User':
+        new_addr = get_user_account_public_key(
+            ch.program_id, 
+            kp.public_key,
+            user_or_stats.user_id
+        )
+    elif type == 'UserStats':
+        new_addr = get_user_stats_account_public_key(
+            ch.program_id, 
+            kp.public_key,
+        )
+
+    return new_addr
+
+modify_and_save_account("State", state_mod)
+modify_and_save_account("User", lambda a: user_user_stats_mod("User", a))
+modify_and_save_account("UserStats", lambda a: user_user_stats_mod("UserStats", a))
+
+with open(keypairs_dir/f'state.secret', 'w') as f: 
+    f.write(state_kp.secret_key.hex())
+
+#%%
+# #%%
+# ## change the admin 
+# lamports = 6674640
+# state_accounts = await ch.program.account["State"].all()
+# assert len(state_accounts) == 1
+# state = state_accounts[0]
+
+# state_path = init_account_dir("State")
+
+# state.account.admin = state_kp.public_key
+
+# anchor_state = Instruction(data=state.account, name="State")
+# data = ch.program.account["State"]._coder.accounts.build(anchor_state)
+# acc_data = base64.b64encode(data).decode("utf-8")
+# account_info = {
+#     'data': [acc_data, 'base64'],
+#     'executable': False,
+#     'lamports': lamports,
+#     'owner': str(ch.program_id),
+#     'rentEpoch': 367
+# }
+# save_account_info(
+#     state_path/(str(state.public_key) + '.json'), 
+#     account_info, 
+#     str(state.public_key)
+# )
 
 #%%
 user_path = init_account_dir("User")
@@ -173,6 +341,7 @@ for user in user_accounts:
     user_stats = [us for us in user_stats_accounts if us.account.authority == user.account.authority][0]
     full_users[str(user.account.authority)] = {"user": user, "user_stats": user_stats}
 
+net_baa = 0
 pk_2_kp = {}
 for old_auth in full_users.keys():
     user = full_users[old_auth]['user']
@@ -186,6 +355,11 @@ for old_auth in full_users.keys():
     # change authority key 
     user_obj.authority = kp.public_key
     user_stats_obj.authority = kp.public_key
+
+    position = [p for p in user_obj.positions if p.market_index == 0 and p.base_asset_amount != 0]
+    if len(position) > 0:
+        assert len(position) == 1
+        net_baa += position[0].base_asset_amount
 
     # rederive pda addresses 
     new_user_pk = get_user_account_public_key(
@@ -234,8 +408,6 @@ for old_auth in full_users.keys():
     with open(keypairs_dir/f'{kp.public_key}.secret', 'w') as f: 
         f.write(kp.secret_key.hex())
 
-with open(keypairs_dir/f'state.secret', 'w') as f: 
-    f.write(state_kp.secret_key.hex())
 
 #%%
 def setup_validator_script(
@@ -256,7 +428,7 @@ def setup_validator_script(
     command = f"solana program dump -u d {program_address} {program_path}"
     os.system(command)
     
-    program_path = f"driftpy/protocol-v2/target/deploy/clearing_house.so"
+    # program_path = f"driftpy/protocol-v2/target/deploy/clearing_house.so"
     validator_str += f' --bpf-program {program_address} {program_path}'
 
     # hard reset 
